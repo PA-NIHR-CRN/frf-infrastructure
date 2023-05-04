16f3ec204b8addca6b05f2778751a0f3ab23b5e0
yousef nasser <yousef.nasser@paconsulting.com>
2023-05-04 16:09:01 +0100

    fix plan

--- main.tf
+++ main.tf
@@ -82,11 +82,11 @@ module "ecr" {
   env       = var.env
 }
 
-module "ses" {
-  source = "./modules/ses"
-  env    = var.env
-  domain = jsondecode(data.aws_secretsmanager_secret_version.terraform_secret_version.secret_string)["domain-name"]
-}
+# module "ses" {
+#   source = "./modules/ses"
+#   env    = var.env
+#   domain = jsondecode(data.aws_secretsmanager_secret_version.terraform_secret_version.secret_string)["domain-name"]
+# }
 
 
 # ## CLOUDFRONT
