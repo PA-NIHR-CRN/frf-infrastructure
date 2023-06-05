68891428184888e95d0137e91530f1ba43bc2d0d
yousef nasser <yousef.nasser@paconsulting.com>
2023-06-05 14:26:18 +0100

    remove brackets

--- modules/container-service/lb.tf
+++ modules/container-service/lb.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "sg-lb" {
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = [var.whitelist_ips]
+    cidr_blocks      = var.whitelist_ips
     ipv6_cidr_blocks = ["::/0"]
   }
 
@@ -17,7 +17,7 @@ resource "aws_security_group" "sg-lb" {
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = [var.whitelist_ips]
+    cidr_blocks      = var.whitelist_ips
     ipv6_cidr_blocks = ["::/0"]
   }
 
