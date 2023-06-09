2e11c5dbbdcb567d98aff1a50a362d4f304f4aff
yousef nasser <yousef.nasser@paconsulting.com>
2023-06-09 15:58:29 +0100

    change http listener to a redirect to https

--- modules/container-service/lb.tf
+++ modules/container-service/lb.tf
@@ -93,10 +93,14 @@ resource "aws_lb_listener" "lb-listener-" {
   protocol          = "HTTP"
 
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.lb-targetgroup.arn
-  }
+    type = "redirect"
 
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
   tags = {
     Name        = "${var.account}-lb-${var.env}-${var.system}-http-listener"
     Environment = var.env
