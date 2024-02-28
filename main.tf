42cc6de7e3db7b2df2f297a8df8127e9a6c3dffa
yousef nasser <yousef.nasser@paconsulting.com>
2024-02-28 16:37:41 +0000

    update provider

--- main.tf
+++ main.tf
@@ -3,7 +3,12 @@ terraform {
     region  = "eu-west-2"
     encrypt = true
   }
-
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
 data "aws_caller_identity" "current" {}
