366081e5598c8ec0229924222940d1a6a158dd1b
yousef nasser <yousef.nasser@paconsulting.com>
2024-04-30 10:58:18 +0100

    test

--- modules/waf/waf.tf
+++ modules/waf/waf.tf
@@ -299,19 +299,18 @@ locals {
     name     = "${var.name}-hostheaderblock",
     priority = var.env == "oat" || var.env == "prod" ? 6 : 5
     action   = "block"
-    scope_down_statement = {
-      not_statement = {
-        byte_match_statement = {
-          field_to_match = {
-            single_header = "host"
-          }
-          positional_constraint = "EXACTLY"
-          search_string         = var.host_url
-          priority              = 0
-          type                  = "NONE"
+    
+    not_statement = {
+      byte_match_statement = {
+        field_to_match = {
+          single_header = "host"
         }
+        positional_constraint = "EXACTLY"
+        search_string         = var.host_url
+        priority              = 0
+        type                  = "NONE"
       }
-      }
+    }
 
     visibility_config = {
       cloudwatch_metrics_enabled = true
